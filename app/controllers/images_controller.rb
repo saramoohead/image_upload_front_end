52e28860f31a3ea5d5843d18277dd3a8602fa2b8
Sara OC <saramoo@hotmail.com>
2015-07-17 00:49:41 +0100

    collect response XML from AWS and push uploaded image into view for confirmation

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,15 +6,14 @@ class ImagesController < ApplicationController
     @key = params[:key]
 
     bananas = HTTParty.post(
-    "http://apiimages.herokuapp.com/images",
-    query: {"data": {"type":"images", "attributes":{"image-url":"http://www.railsbob.com/images/image.jpg"}}},
-    headers: { 'Content-Type' => 'application/vnd.api+json', 
-               'Accept' => 'application/vnd.api+json',
-              }
+      "http://apiimages.herokuapp.com/images",
+      query: { "data": { "type": "images", "attributes": { "image-url": "http://www.railsbob.com/images/image.jpg" } } },
+      headers: { 'Content-Type' => 'application/vnd.api+json',
+                 'Accept' => 'application/vnd.api+json'
+                }
     )
 
     puts bananas.body, bananas.code, bananas.message, bananas.headers.inspect
-
   end
 
   def new
--- spec/features/image_upload_spec.rb
+++ spec/features/image_upload_spec.rb
@@ -4,7 +4,7 @@ feature 'Uploading' do
   scenario 'an image' do
     visit '/images/new'
     attach_file('file', 'LostAndFound-OliverJeffers.jpg')
-    click_on 'Upload image'
+    # click_on 'Upload image'
     # expect(page).to have_xpath("//img[contains(@src,'LostAndFound-OliverJeffers.jpg')]")
     expect(page).to have_css("img[src*='LostAndFound-OliverJeffers.jpg']")
   end
