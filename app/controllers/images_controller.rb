80f3416546aa7a74c7cfebf19c01a3bdc96f6c4d
Sara OC <saramoo@hotmail.com>
2015-07-26 22:15:14 +0100

    pass image_id between partials to add caption via dynamically created patch request

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,17 +3,30 @@ require 'httparty'
 class ImagesController < ApplicationController
 
   def index
+
     @key = params[:key]
 
-    bananas = HTTParty.post(
-      "http://apiimages.herokuapp.com/images",
-      query: { "data": { "type": "images", "attributes": { "image-url": "https://imagesapi.s3.amazonaws.com/#{@key}" } } },
-      headers: { 'Content-Type' => 'application/vnd.api+json',
-                 'Accept' => 'application/vnd.api+json'
-                }
-    )
+    if @key != nil
+      image_to_api = HTTParty.post(
+        "http://apiimages.herokuapp.com/images",
+        query: { "data": { "type": "images", "attributes": { "image-url": "https://imagesapi.s3.amazonaws.com/#{@key}", "caption": "  " } } },
+        headers: { 'Content-Type' => 'application/vnd.api+json',
+                   'Accept' => 'application/vnd.api+json'
+                  }
+      )
 
-    puts bananas.body, bananas.code, bananas.message, bananas.headers.inspect
+      puts image_to_api.body, image_to_api.code, image_to_api.message, image_to_api.headers.inspect
+
+      if image_to_api.code != 503
+        response_from_api = JSON.parse(image_to_api)
+        @id = response_from_api["data"]["id"]
+        @image_id = @id
+      else
+        @id = 84
+      end
+
+    end
+
   end
 
   def new
@@ -21,16 +34,17 @@ class ImagesController < ApplicationController
 
   def create
     @caption = params[:caption]
+    @image_id = params[:image_id]
 
-    apples = HTTParty.patch(
-      "http://apiimages.herokuapp.com/images/63",
-      query: { "data": { "type": "images", "id": "63", "attributes": {"caption": "#{@caption}" } } },
+    caption_to_api = HTTParty.patch(
+      "http://apiimages.herokuapp.com/images/#{@image_id}",
+      query: { "data": { "type": "images", "id": "#{@image_id}", "attributes": {"caption": "#{@caption}" } } },
       headers: { 'Content-Type' => 'application/vnd.api+json',
                  'Accept' => 'application/vnd.api+json'
                 }
     )
 
-    puts apples.body, apples.code, apples.message, apples.headers.inspect
+    puts caption_to_api.body, caption_to_api.code, caption_to_api.message, caption_to_api.headers.inspect
 
     redirect_to new_image_path
   end
